15e370d15b0c5197b82d12ccdcd8d9894f527ec3
sebastien <moi@mail.com>
2018-10-09 09:37:39 +0200

    Création jeu animaux

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,74 +8,143 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   private animalList = [
+    {
+      'title': 'Vache',
+      'image': 'imgs/animals/cow-icon.png',
+      'desc': 'Meugle',
+      'file': '/sounds/cow.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Dauphin',
+      'image': 'imgs/animals/dolphin-icon.png',
+      'desc': 'Siffle',
+      'file': '/sounds/dolphin.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Grenouille',
+      'image': 'imgs/animals/frog-icon.png',
+      'desc': 'Coasse',
+      'file': '/sounds/frog.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Oiseau',
+      'image': 'imgs/animals/bird-icon.png',
+      'desc': 'Chante',
+      'file': '/sounds/bird.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Cochon',
+      'image': 'imgs/animals/pig-icon.png',
+      'desc': 'Grogne',
+      'file': '/sounds/pig.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Chien',
+      'image': 'imgs/animals/puppy-icon.png',
+      'desc': 'Aboie',
+      'file': '/sounds/dog.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Chat',
+      'image': 'imgs/animals/black-cat-icon.png',
+      'desc': 'Miaule',
+      'file': '/sounds/cat.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Cheval',
+      'image': 'imgs/animals/horse-icon.png',
+      'desc': 'Hennit',
+      'file': '/sounds/horse.wav',
+      'playing': false
+    },
+    {
+      'title': 'Ane',
+      'image': 'imgs/animals/donkey-icon.png',
+      'desc': 'Brait',
+      'file': '/sounds/donkey.wav',
+      'playing': false
+    }
+  ];
+
+  private choosenAnimal;
+
+  private choosenIndex;
+
+  private media = null;
+
+  private showReorder = false;
+  
+   constructor(public toastCtrl: ToastController) {
+  }
+
+  onAnimalClick(animal) {
+
+    let toastText;
+    let toastClass;
+
+    if(!this.choosenAnimal){
+      toastText = " Cliques d'abord sur JOUER pour entendre un animal";
+      toastClass = "";
+    }else if (this.choosenAnimal == animal) {
+      toastText = "Tu as gagné";
+      toastClass = "goodAnswer";
+
+      this.choosenAnimal.playing = false;
+      this.choosenAnimal = null;
+      this.media = null;
+    } else {
+      toastText = "Essaie encore";
+      toastClass = "badAnswer";
+    }
+
+    this.toastCtrl.create(
       {
-        'title': 'Vache',
-        'image': 'img/animals/cow-icon.png',
-        'desc': 'Meugle',
-        'file': '/sounds/cow.mp3',
-        'playing': false
-      },
-      {
-        'title': 'Dauphin',
-        'image': 'img/animals/dolphin-icon.png',
-        'desc': 'Siffle',
-        'file': '/sounds/dolphin.mp3',
-        'playing': false
-      },
-      {
-        'title': 'Grenouille',
-        'image': 'img/animals/frog-icon.png',
-        'desc': 'Coasse',
-        'file': '/sounds/frog.mp3',
-        'playing': false
-      },
-      {
-        'title': 'Oiseau',
-        'image': 'img/animals/bird-icon.png',
-        'desc': 'Chante',
-        'file': '/sounds/bird.mp3',
-        'playing': false
-      },
-      {
-        'title': 'Cochon',
-        'image': 'img/animals/pig-icon.png',
-        'desc': 'Grogne',
-        'file': '/sounds/pig.mp3',
-        'playing': false
-      },
-      {
-        'title': 'Chien',
-        'image': 'img/animals/puppy-icon.png',
-        'desc': 'Aboie',
-        'file': '/sounds/dog.mp3',
-        'playing': false
-      },
-      {
-        'title': 'Chat',
-        'image': 'img/animals/black-cat-icon.png',
-        'desc': 'Miaule',
-        'file': '/sounds/cat.mp3',
-        'playing': false
-      },
-      {
-        'title': 'Cheval',
-        'image': 'img/animals/horse-icon.png',
-        'desc': 'Hennit',
-        'file': '/sounds/horse.wav',
-        'playing': false
-      },
-      {
-        'title': 'Ane',
-        'image': 'img/animals/donkey-icon.png',
-        'desc': 'Brait',
-        'file': '/sounds/donkey.wav',
-        'playing': false
+        message: toastText,
+        duration: 1500,
+        position: "middle",
+        cssClass: toastClass
       }
-    ];
+    ).present();
+  }
 
+  playSound() {
 
-  constructor(public navCtrl: NavController) {
+    if (this.media && (this.media.currentTime < this.media.duration)) {
+      this.media.pause();
+      if (this.choosenAnimal) {
+        this.choosenAnimal.playing = false;
+      }
+    }
+
+    let index = Math.floor(Math.random() * this.animalList.length);
+    this.choosenAnimal = this.animalList[index];
+    this.choosenIndex = index;
+
+    this.media = new Audio();
+    this.media.src = "assets" + this.choosenAnimal.file;
+    this.media.load();
+    this.media.play();
+
+    this.choosenAnimal.playing = true;
+
+    var that = this;
+    this.media.ontimeupdate = function () {
+      if (this.ended && that.choosenAnimal) {
+        that.choosenAnimal.playing = false;
+      }
+
+    }
 
   }
 
+  deleteAnimal(pos){
+    this.animalList.splice(pos, 1);
+  }
 }
